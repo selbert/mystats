d4d23f9066c4286bd3fcf0bcc93928a5b4c90d7e
Thomas Selber <thomas.selber@born.ch>
2015-10-15 15:29:53 +0100

    added stuff

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 var express = require('express');
-var fs = require("fs");
-var sqlite3 = require("sqlite3").verbose();
+var rest = require('./rest-routing.js');
 
-var file = "../load.sql";
-var exists = fs.existsSync(file);
-var db = new sqlite3.Database(file);
 
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
+
+app.use('/rest', rest);
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
-
-db.serialize(function() {
-  db.each("SELECT AVG(avg) AS avg FROM load", function(err, row) {
-    console.log("Average load: " + row.avg);
-  });
-});
-
-db.close();
