b5b9499eb161afe717f4c60cd631c111a6ffb806
Thomas Selber <thomas.selber@born.ch>
2015-10-15 11:41:26 +0200

    added sqlite3 access

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var express = require('express');
+var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
+
+var file = "../load.sql";
+var exists = fs.existsSync(file);
+var db = new sqlite3.Database(file);
+
 var app = express();
 
 app.get('/', function (req, res) {
@@ -11,3 +18,11 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+db.serialize(function() {
+  db.each("SELECT AVG(avg) AS avg FROM load", function(err, row) {
+    console.log("Average load: " + row.avg);
+  });
+});
+
+db.close();
